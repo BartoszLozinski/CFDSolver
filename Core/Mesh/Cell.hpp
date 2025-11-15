803832fe85869397680187a033718df67ba78df6
Bartosz Lozinski <lozinski.bart@gmail.com>
2025-11-15 21:42:22 +0100

    Removed unnecessary space

--- Core/Mesh/Cell.hpp
+++ Core/Mesh/Cell.hpp
@@ -1,7 +1,6 @@
 #pragma once
 #include "Face.hpp"
 
-
 /*
 This is simple solver just to learn a little bit about numerical methods, multithreading, maybe gpu acceleration
 It is not generic yet. Probably will be expanded to be possible to import external meshes in the future
